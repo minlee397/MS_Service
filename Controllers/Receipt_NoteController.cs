3273f9f8a525bc09b2cfa1400214333e6c8c82a1
minlee397 <jimmi2051@gmail.com>
2018-12-19 22:03:19 +0700

    update_2

--- Controllers/Receipt_NoteController.cs
+++ Controllers/Receipt_NoteController.cs
@@ -52,7 +52,7 @@ namespace QL_DT.Controllers
                 _DTContext.SaveChanges();
                 using (HttpClient client = new HttpClient())
                 {
-                    client.BaseAddress = new Uri("http://localhost:8080");
+                    client.BaseAddress = new Uri("http://localhost:8081");
                     MediaTypeWithQualityHeaderValue contentType = new MediaTypeWithQualityHeaderValue("application/json");
                     client.DefaultRequestHeaders.Accept.Add(contentType);
                     var obj = new
